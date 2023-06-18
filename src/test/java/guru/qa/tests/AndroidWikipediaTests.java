4a7bbfb2f3d8e7dc86c8d533a4f0df52ead6494a
lebedevich-es <lebedevich.es@gmail.com>
2023-06-18 09:46:47 +0200

    updated android tests

--- src/test/java/guru/qa/tests/AndroidWikipediaTests.java
+++ src/test/java/guru/qa/tests/AndroidWikipediaTests.java
@@ -33,7 +33,22 @@ public class AndroidWikipediaTests extends TestBase {
         step("Click on article header ", () ->
                 $(AppiumBy.id("org.wikipedia.alpha:id/horizontal_scroll_list_item_text")).click());
         step("Check opening", () ->
-                $(AppiumBy.id("org.wikipedia.alpha:id/view_news_fullscreen_story_text"))
-                        .shouldHave(text("At least 78 people are killed and hundreds of others are missing after a migrant boat sinks off the coast of Pylos, Greece.")));
+                $$(AppiumBy.id("org.wikipedia.alpha:id/view_news_fullscreen_link_card_list"))
+                        .shouldHave(sizeGreaterThan(0)));
+    }
+
+    @Test
+    @Tag("android")
+    @DisplayName("Open article from Search")
+    void openArticleFromSearchTest() {
+        step("Type search", () -> {
+            $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("java");
+        });
+        step("Open first article", () ->
+                $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_container")).first().click());
+        step("Check opening", () ->
+                $(AppiumBy.id("org.wikipedia.alpha:id/view_wiki_error_text"))
+                        .shouldHave(text("An error occurred")));
     }
 }
